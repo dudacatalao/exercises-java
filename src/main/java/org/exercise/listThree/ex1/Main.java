85e251a39ecc6e66c96fcdde2c12a81f4ffd3d7d
dudacatalao <dmelocatalao@gmail.com>
2024-04-03 16:49:26 -0300

    updates

--- src/main/java/org/exercise/listThree/ex1/Main.java
+++ src/main/java/org/exercise/listThree/ex1/Main.java
@@ -1,70 +1,100 @@
 package org.exercise.listThree.ex1;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("\n" +
+                "███╗░░██╗██╗░░░██╗███╗░░░███╗██████╗░███████╗██████╗░░██████╗\n" +
+                "████╗░██║██║░░░██║████╗░████║██╔══██╗██╔════╝██╔══██╗██╔════╝\n" +
+                "██╔██╗██║██║░░░██║██╔████╔██║██████╦╝█████╗░░██████╔╝╚█████╗░\n" +
+                "██║╚████║██║░░░██║██║╚██╔╝██║██╔══██╗██╔══╝░░██╔══██╗░╚═══██╗\n" +
+                "██║░╚███║╚██████╔╝██║░╚═╝░██║██████╦╝███████╗██║░░██║██████╔╝\n" +
+                "╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚═════╝░╚══════╝╚═╝░░╚═╝╚═════╝░");
+
         ArrayList<Integer> evenNumbers = new ArrayList<>();
         ArrayList<Integer> oddNumbers = new ArrayList<>();
         ArrayList<Integer> negativeNumbers = new ArrayList<>();
         ArrayList<Integer> positiveNumbers = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Insert a number: ");
-            int number = sc.nextInt();
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.print("Insert a number: ");
+                int number = sc.nextInt();
 
-            if(number % 2 == 0){
-                evenNumbers.add(number);
-            } else{
-                oddNumbers.add(number);
+                if(number % 2 == 0){
+                    evenNumbers.add(number);
+                } else{
+                    oddNumbers.add(number);
+                }
+
+                if(number > 0){
+                    positiveNumbers.add(number);
+                }else{
+                    negativeNumbers.add(number);
+                }
             }
-
-            if(number > 0){
-                positiveNumbers.add(number);
-            }else{
-                negativeNumbers.add(number);
-            }
-
+        }catch (InputMismatchException e){
+            System.out.println("Invalid data, insert a Integer number.");
         }
 
-        System.out.print("[1] Even numbers\n[2] Odd numbers\n[3] Negative numbers\n[4] Positive numbers\nWhich option do you want to see?\n-> ");
-        int option = sc.nextInt();
+        System.out.println("-----------------------------------------------------------");
+        System.out.print("[A] Even numbers\n[B] Odd numbers\n[C] Negative numbers\n[D] Positive numbers\n[E] Exit\nWhich option do you want to see?\n-> ");
+        String option = sc.next().toUpperCase();
+
+        try {
+            System.out.println("Loading..");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         switch (option){
-            case 1:
+            case "A":
                 System.out.println("Even Numbers:");
                 for (int i = 0; i < evenNumbers.size(); i++) {
-                    System.out.println(evenNumbers.get(i));
+                    System.out.print(evenNumbers.get(i));
+                    if (i < evenNumbers.size() - 1) {
+                        System.out.print(", ");
+                    }
                 }
                 break;
-            case 2:
+            case "B":
                 System.out.println("Odd Numbers:");
                 for (int i = 0; i < oddNumbers.size(); i++) {
-                    System.out.println(oddNumbers.get(i));
+                    System.out.print(oddNumbers.get(i));
+                    if (i < oddNumbers.size() - 1) {
+                        System.out.print(", ");
+                    }
                 }
                 break;
-            case 3:
-                System.out.println("Negative Numbers:");
+            case "C":
+                System.out.println("Negative numbers:");
                 for (int i = 0; i < negativeNumbers.size(); i++) {
-                    System.out.println(negativeNumbers.get(i));
+                    System.out.print(negativeNumbers.get(i));
+                    if (i < negativeNumbers.size() - 1) {
+                        System.out.print(", ");
+                    }
                 }
                 break;
-            case 4:
-                System.out.println("Positive Numbers:");
+            case "D":
+                System.out.println("Positive numbers:");
                 for (int i = 0; i < positiveNumbers.size(); i++) {
-                    System.out.println(positiveNumbers.get(i));
+                    System.out.print(positiveNumbers.get(i));
+                    if (i < positiveNumbers.size() - 1) {
+                        System.out.print(", ");
+                    }
                 }
                 break;
-        }
+            case "E":
+                System.out.println("Finishing...");
+                System.exit(0);
 
+            default:
+                System.out.println("Invalid option.");
+        }
     }
 }
-
-
-//FUP que peça 10 números ao usuário. Em seguida, pergunte a ele o que ele deseja saber (utilizando a estrutura switch):
-//a. Quantos números pares?
-//b. Quantos números ímpares?
-//c. Quantos negativos?
-//d. Quantos positivos?
---
+++ src/main/java/org/exercise/listThree/ex2/Main.java
@@ -0,0 +1,16 @@
+package org.exercise.listThree.ex2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("");
+    }
+}
+
+//FUP que peça a idade, a altura e o peso de 10 pessoas e mostre na tela:
+//a. Quantas tem idade acima de 50 anos
+//b. Quantas tem altura acima de 1.60m
+//c. Quantas tem peso abaixo de 80kg
