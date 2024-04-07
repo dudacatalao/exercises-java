8544667764b9a8903513e73de4137387ae8295a9
Maria Eduarda <dmelocatalao@gmail.com>
2024-04-07 17:10:47 -0300

    updates

--- src/main/java/org/exercise/listThree/ex5/Main.java
+++ src/main/java/org/exercise/listThree/ex5/Main.java
@@ -1,30 +1,57 @@
 package org.exercise.listThree.ex5;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        boolean continueInsertion = true;
+        int positive = 0;
+        int negative = 0;
 
         do {
-            System.out.print("Insert the balance: R$");
-            float firstBalance = sc.nextFloat();
+            try{
 
-            System.out.println("Do you want to insert more balance?");
+            }catch (InputMismatchException e){
+
+            }
+            System.out.print("Insert the balance: R$");
+            float balance = sc.nextFloat();
+
+            System.out.println("Do you want to insert more balance? (yes/no)");
             String option = sc.next().toLowerCase();
 
-        }while (){
+            if (balance > 0){
+                positive++;
+            }else{
+                negative++;
+            }
 
+            if (option.equals("no")) {
+                continueInsertion = false;
+            }
+
+        } while (continueInsertion);
+
+        int total = positive + negative;
+        double porcentPositive = ((double) positive / total) * 100;
+        double porcentNegative = ((double) negative / total) * 100;
+
+        if (porcentPositive > porcentNegative){
+            System.out.println("No risk!");
+        }else{
+            System.out.println("Risk to the bank.");
         }
-
 
 
     }
 }
 
-//FUP que peça o saldo de várias pessoas.
-// Ao final, mostre quantas delas apre-sentam saldo negativo e quantas delas positivos.
-// Porém, deve ser classificado da seguinte forma:
-// Se 50% ou mais do total de pessoas apresentaram saldo positivo, deve ser mostrado “Nenhum risco!”,
-// caso contrário deve ser mostrado “Risco ao banco!”.
-// Após informar o saldo da primeira pessoa, o programa deverá perguntar se o usuário deseja continuar informando saldos ou encerrar, mostrando a men-sagem.
+//FUP que peça o saldo de várias pessoas. Ao final, mostre quantas delas apre-
+//sentam saldo negativo e quantas delas positivos. Porém, deve ser classificado da
+//seguinte forma: Se 50% ou mais do total de pessoas apresentaram saldo positivo,
+//deve ser mostrado “Nenhum risco!”, caso contrário deve ser mostrado “Risco ao
+//banco!”. Após informar o saldo da primeira pessoa, o programa deverá perguntar
+//se o usuário deseja continuar informando saldos ou encerrar, mostrando a men-
+//sagem.
