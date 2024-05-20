1acaa433a187e1f5e173cc90cd091c0755f3b9c1
dudacatalao <dmelocatalao@gmail.com>
2024-05-20 16:50:05 -0300

    updates

--- src/main/java/org/exercise/listFive/ex3/Loan.java
+++ src/main/java/org/exercise/listFive/ex3/Loan.java
@@ -1,11 +1,33 @@
 package org.exercise.listFive.ex3;
 
 public class Loan {
-    double currentSalary;
     double valueLoan;
+    double totalValue;
+    double portionValue;
 
     public Loan(InputOutput inputOutput){
-        this.currentSalary = inputOutput.currentSalary;
         this.valueLoan = inputOutput.valueLoan;
+    }
+
+    double getTotalValue(){
+        return this.totalValue = this.valueLoan + ( this.valueLoan * 3.5);
+    }
+
+    double getPortionValue(){
+        return this.portionValue = this.totalValue / 24;
+    }
+
+    String returnValues(){
+        return String.format(
+                "------------ Overview  of the Loan------------\n" +
+                        "| Your salary:                   R$ %.2f        |\n" +
+                        "| Total amount with interest:    R$ %.2f        |\n" +
+                        "| IRPF discount:        R$ %.2f        |\n" +
+                        "| Health Plan:          R$ %.2f        |\n" +
+                        "| Additional Hours:     R$ %.f        |\n" +
+                        "| TOTAL SALARY:         R$ %.2f        |\n" +
+                        "-----------------------------------------",
+                this.salary, discountINSS(), discountIRPF(), healthPlan(), additionExtraHour(), calculingFinalSalary()
+        );
     }
 }
